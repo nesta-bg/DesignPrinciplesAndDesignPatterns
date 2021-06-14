64090e37ba31effcae2e2aad53412d49bc09d6d3
nesta-bg <nenadnesta@gmail.com>
2021-06-14 14:39:25 +0200

    Update DecoratorDesignPattern.cs

--- Design Patterns/Structural Design Patterns/DecoratorDesignPattern.cs
+++ Design Patterns/Structural Design Patterns/DecoratorDesignPattern.cs
@@ -4,93 +4,94 @@ namespace DecoratorDP
 {
 	//Component
 	public interface ICar
-    {
-        string Make { get; }
-        double GetPrice();
-    }
+    	{
+        	string Make { get; }
+        	double GetPrice();
+    	}
 	
 	//ConcreteComponent
 	public sealed class Hyndai : ICar
-    {
-        public string Make
-        {
-            get { return "HatchBack"; }
-        }
+    	{
+        	public string Make
+        	{
+            		get { return "HatchBack"; }
+        	}
 		
-        public double GetPrice()
-        {
-            return 800000;
-        }
-    }
+        	public double GetPrice()
+        	{
+            		return 800000;
+        	}
+    	}
 	
 	//ConcreteComponent
 	public sealed class Suzuki : ICar
-    {
-        public string Make
-        {
-            get { return "Sedan"; }
-        }
-        public double GetPrice()
-        {
-            return 1000000;
-        }
-    }
+    	{
+        	public string Make
+        	{
+            		get { return "Sedan"; }
+        	}
+		
+        	public double GetPrice()
+        	{
+            		return 1000000;
+        	}
+    	}
 	
 	//=======================================================//
 	
 	//Decorator
 	public abstract class CarDecorator : ICar
-    {
-        private ICar car;
+    	{
+        	private ICar car;
 		
-        public CarDecorator(ICar Car)
-        {
-            car = Car;
-        }
+        	public CarDecorator(ICar Car)
+        	{
+            		car = Car;
+        	}
 		
-        public string Make { get { return car.Make; } }
+        	public string Make { get { return car.Make; } }
 
-        public double GetPrice()
-        {
-            return car.GetPrice();
-        }
+        	public double GetPrice()
+        	{
+            		return car.GetPrice();
+        	}
 		
-        public abstract double GetDiscountedPrice();
-    }
+        	public abstract double GetDiscountedPrice();
+    	}
 	
 	//ConcreteDecorator
-    public class OfferPrice : CarDecorator
-    {
-        public OfferPrice(ICar car) 
+    	public class OfferPrice : CarDecorator
+    	{
+        	public OfferPrice(ICar car) 
 			: base(car)
-        {
-        }
+        	{
+        	}
 		
-        public override double GetDiscountedPrice()
-        {
-            return .8 * base.GetPrice();
-        }
-    }
+        	public override double GetDiscountedPrice()
+        	{
+            		return .8 * base.GetPrice();
+        	}
+    	}
 	
 	public class Program
 	{
 		public static void Main()
 		{
 			ICar car = new Suzuki();
-            CarDecorator decorator = new OfferPrice(car);
+            		CarDecorator decorator = new OfferPrice(car);
             
 			Console.WriteLine(string.Format("Make :{0}  Price:{1} " +
-                "DiscountPrice : {2}"
-                , decorator.Make,  decorator.GetPrice(),
-                decorator.GetDiscountedPrice()));
+                		"DiscountPrice : {2}"
+                		, decorator.Make,  decorator.GetPrice(),
+                		decorator.GetDiscountedPrice()));
 			
-            ICar car2 = new Hyndai();
+            		ICar car2 = new Hyndai();
 			CarDecorator decorator2 = new OfferPrice(car2);
 			
 			Console.WriteLine(string.Format("Make :{0}  Price:{1} " +
-                "DiscountPrice : {2}"
-                , decorator2.Make,  decorator2.GetPrice(),
-                decorator2.GetDiscountedPrice()));
+                		"DiscountPrice : {2}"
+                		, decorator2.Make,  decorator2.GetPrice(),
+                		decorator2.GetDiscountedPrice()));
 			
 			Console.WriteLine();
 		}	
