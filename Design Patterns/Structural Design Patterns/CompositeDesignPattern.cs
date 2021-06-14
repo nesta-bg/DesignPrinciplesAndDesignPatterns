4ca7a9d50a85a27d36e775998aabd24189a1c90f
nesta-bg <nenadnesta@gmail.com>
2021-06-14 14:09:32 +0200

    Update CompositeDesignPattern.cs

--- Design Patterns/Structural Design Patterns/CompositeDesignPattern.cs
+++ Design Patterns/Structural Design Patterns/CompositeDesignPattern.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 namespace CompositeDP
 {
 	//Component
-    public abstract class EmployeeComp
-    {
-		public string Name;
+    	public abstract class EmployeeComp
+    	{
+    		public string Name;
 		public string Department;
 		
 		public EmployeeComp(string name, string dept)
@@ -16,68 +16,69 @@ namespace CompositeDP
 		}
 		
 		public abstract void GetDetails(int indentation);
-    }
+    	}
 	
 	//Leaf
-	public class Employee : EmployeeComp
-    {
-        public Employee(string name, string dept)
+    	public class Employee : EmployeeComp
+    	{
+        	public Employee(string name, string dept)
 			:base(name, dept)
-        {
+        	{
 			
-        }
+        	}
 		
-        public override void GetDetails(int indentation)
-        {
-            Console.WriteLine(string.Format("{0} Name:{1}, Dept:{2} (Leaf) ",
-                new String('-', indentation), this.Name, this.Department));
-        }
-    }
+        	public override void GetDetails(int indentation)
+        	{
+            		Console.WriteLine(string.Format("{0} Name:{1}, Dept:{2} (Leaf) ",
+                		new String('-', indentation), this.Name, this.Department));
+        	}
+    	}
 	
-	//Composite
-	public class Manager : EmployeeComp
-    {
-        public List<EmployeeComp> SubOrdinates = new List<EmployeeComp>();
+    	//Composite
+    	public class Manager : EmployeeComp
+    	{
+        	public List<EmployeeComp> SubOrdinates = new List<EmployeeComp>();
 		
-        public Manager(string name, string dept)
+        	public Manager(string name, string dept)
 			:base(name, dept)
-        {
+        	{
 			
-        }
+        	}
        
-        public override void GetDetails(int indentation)
-        {
-            Console.WriteLine();
-            Console.WriteLine(string.Format("{0}+ Name:{1}, " + "Dept:{2} - Manager(Composite)",
-                new String('*', indentation), this.Name, this.Department));
-            foreach (EmployeeComp component in SubOrdinates)
-            {
-                component.GetDetails(indentation + 3);
-            }
-        }
-    }
+        	public override void GetDetails(int indentation)
+        	{
+            		Console.WriteLine();
+            		Console.WriteLine(string.Format("{0}+ Name:{1}, " + "Dept:{2} - Manager(Composite)",
+                		new String('*', indentation), this.Name, this.Department));
+            		foreach (EmployeeComp component in SubOrdinates)
+            		{
+                		component.GetDetails(indentation + 3);
+            		}
+        	}
+    	}
 
 	public class Program
 	{
 		public static void Main()
 		{
 			EmployeeComp John = new Employee("John", "IT");
-            EmployeeComp Mike = new Employee("Mike", "IT");
-            EmployeeComp Jason = new Employee("Jason", "HR");
-            EmployeeComp Eric = new Employee("Eric", "HR");
-            EmployeeComp Henry = new Employee("Henry", "HR");
+            		EmployeeComp Mike = new Employee("Mike", "IT");
+            		EmployeeComp Jason = new Employee("Jason", "HR");
+            		EmployeeComp Eric = new Employee("Eric", "HR");
+            		EmployeeComp Henry = new Employee("Henry", "HR");
 
-            EmployeeComp James = new Manager("James", "IT") { SubOrdinates = { John, Mike } };
-            EmployeeComp Philip = new Manager("Philip", "HR") { SubOrdinates = { Jason, Eric, Henry } };
+            		EmployeeComp James = new Manager("James", "IT") { SubOrdinates = { John, Mike } };
+            		EmployeeComp Philip = new Manager("Philip", "HR") { SubOrdinates = { Jason, Eric, Henry } };
 			
-            EmployeeComp Bob = new Manager("Bob", "Head") { SubOrdinates = { James, Philip } };
+            		EmployeeComp Bob = new Manager("Bob", "Head") { SubOrdinates = { James, Philip } };
 			
 			James.GetDetails(1);
 			Console.WriteLine("********************");
 			Philip.GetDetails(1);
 			Console.WriteLine("********************");
-            Bob.GetDetails(1);
+            		Bob.GetDetails(1);
 			Console.WriteLine("********************");
-            Console.ReadLine();
+            		Console.ReadLine();
 		}	
 	}
+}
