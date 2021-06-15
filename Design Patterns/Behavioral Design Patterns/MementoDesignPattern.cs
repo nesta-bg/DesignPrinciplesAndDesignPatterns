d63808827c1563887e8fe5c8123c1fb778d81827
nesta-bg <nenadnesta@gmail.com>
2021-06-15 17:00:34 +0200

    Update MementoDesignPattern.cs

--- Design Patterns/Behavioral Design Patterns/MementoDesignPattern.cs
+++ Design Patterns/Behavioral Design Patterns/MementoDesignPattern.cs
@@ -5,107 +5,107 @@ namespace MementoDP
 	/// The 'Originator' class
 	class SalesProspect
   	{
-    	private string _name;
-    	private string _phone;
-    	private double _budget;
+    		private string _name;
+    		private string _phone;
+    		private double _budget;
  
-    	// Gets or sets name
-    	public string Name
-    	{
-      		get { return _name; }
-      		set
-      		{
-        		_name = value;
-        		Console.WriteLine("Name:  " + _name);
-      		}
-    	}
+    		// Gets or sets name
+    		public string Name
+    		{	
+      			get { return _name; }
+      			set
+      			{
+        			_name = value;
+        			Console.WriteLine("Name:  " + _name);
+      			}
+    		}
  
-    	// Gets or sets phone
-    	public string Phone
-    	{
-      		get { return _phone; }
-      		set
-      		{
-        		_phone = value;
-        		Console.WriteLine("Phone: " + _phone);
-      		}
-    	}
+    		// Gets or sets phone
+    		public string Phone
+    		{
+      			get { return _phone; }
+      			set
+      			{
+        			_phone = value;
+        			Console.WriteLine("Phone: " + _phone);
+      			}
+    		}
  
-    	// Gets or sets budget
-    	public double Budget
-    	{
-      		get { return _budget; }
-      		set
-      		{
-        		_budget = value;
-        		Console.WriteLine("Budget: " + _budget);
-      		}
-    	}
+    		// Gets or sets budget
+    		public double Budget
+    		{
+      			get { return _budget; }
+      			set
+      			{
+        			_budget = value;
+        			Console.WriteLine("Budget: " + _budget);
+      			}
+    		}
  
 		// Stores memento
-    	public Memento SaveMemento()
-    	{
-      		Console.WriteLine("\nSaving state --\n");
-      		return new Memento(_name, _phone, _budget);
-    	}
+    		public Memento SaveMemento()
+    		{
+      			Console.WriteLine("\nSaving state --\n");
+      			return new Memento(_name, _phone, _budget);
+    		}
  
-    	// Restores memento
-    	public void RestoreMemento(Memento memento)
-    	{
-      		Console.WriteLine("\nRestoring state --\n");
-      		this.Name = memento.Name;
-      		this.Phone = memento.Phone;
-      		this.Budget = memento.Budget;
-    	}
+    		// Restores memento
+    		public void RestoreMemento(Memento memento)
+    		{
+      			Console.WriteLine("\nRestoring state --\n");
+      			this.Name = memento.Name;
+      			this.Phone = memento.Phone;
+      			this.Budget = memento.Budget;
+    		}
   	}
 	
 	/// The 'Memento' class
 	class Memento
   	{
-    	private string _name;
-    	private string _phone;
-    	private double _budget;
+    		private string _name;
+    		private string _phone;
+    		private double _budget;
  
-    	// Constructor
-    	public Memento(string name, string phone, double budget)
-    	{
-      		this._name = name;
-      		this._phone = phone;
-      		this._budget = budget;
-    	}
+    		// Constructor
+    		public Memento(string name, string phone, double budget)
+    		{
+      			this._name = name;
+      			this._phone = phone;
+      			this._budget = budget;
+    		}
  
-    	// Gets or sets name
+    		// Gets or sets name
 		public string Name
 		{
-		  get { return _name; }
-		  set { _name = value; }
+			get { return _name; }
+		  	set { _name = value; }
 		}
 
 		// Gets or set phone
 		public string Phone
 		{
-		  get { return _phone; }
-		  set { _phone = value; }
+			get { return _phone; }
+		  	set { _phone = value; }
 		}
 
 		// Gets or sets budget
 		public double Budget
 		{
-		  get { return _budget; }
-		  set { _budget = value; }
+			get { return _budget; }
+		  	set { _budget = value; }
 		}
 	}
  
 	/// The 'Caretaker' class
 	class ProspectMemory
   	{
-    	private Memento _memento;
+    		private Memento _memento;
  
 		// Property
 		public Memento Memento
 		{
-		  set { _memento = value; }
-		  get { return _memento; }
+			set { _memento = value; }
+		  	get { return _memento; }
 		}
 	}
 	
@@ -121,8 +121,8 @@ namespace MementoDP
 			
 			/// The 'Caretaker' class
 			// Store internal state
-      		ProspectMemory m = new ProspectMemory();
-      		m.Memento = s.SaveMemento();
+      			ProspectMemory m = new ProspectMemory();
+      			m.Memento = s.SaveMemento();
  
 			// Continue changing originator
 			s.Name = "Leo Welch";
