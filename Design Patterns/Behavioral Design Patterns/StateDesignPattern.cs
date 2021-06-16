b96bdf2258afe2c7d5b8275de5c9c712cf0cd13c
nesta-bg <nenadnesta@gmail.com>
2021-06-16 11:34:57 +0200

    Update StateDesignPattern.cs

--- Design Patterns/Behavioral Design Patterns/StateDesignPattern.cs
+++ Design Patterns/Behavioral Design Patterns/StateDesignPattern.cs
@@ -15,14 +15,14 @@ namespace StateDP
 		// Properties
 		public Account Account
 		{
-		  get { return account; }
-		  set { account = value; }
+			get { return account; }
+		  	set { account = value; }
 		}
 
 		public double Balance
 		{
-		  get { return balance; }
-		  set { balance = value; }
+			get { return balance; }
+		  	set { balance = value; }
 		}
 
 		public abstract void Deposit(double amount);
@@ -43,43 +43,43 @@ namespace StateDP
 		// Constructor
 		public RedState(State state)
 		{
-		  this.balance = state.Balance;
-		  this.account = state.Account;
-		  Initialize();
+			this.balance = state.Balance;
+		  	this.account = state.Account;
+		  	Initialize();
 		}
 
 		private void Initialize()
 		{
-		  // Should come from a datasource
-		  interest = 0.0;
-		  lowerLimit = -100.0;
-		  upperLimit = 0.0;
-		  _serviceFee = 15.00;
+			// Should come from a datasource
+		  	interest = 0.0;
+		  	lowerLimit = -100.0;
+		  	upperLimit = 0.0;
+		  	_serviceFee = 15.00;
 		}
 
 		public override void Deposit(double amount)
 		{
-		  balance += amount;
-		  StateChangeCheck();
+			balance += amount;
+		  	StateChangeCheck();
 		}
 
 		public override void Withdraw(double amount)
 		{
-		  amount = amount - _serviceFee;
-		  Console.WriteLine("No funds available for withdrawal!");
+			amount = amount - _serviceFee;
+		  	Console.WriteLine("No funds available for withdrawal!");
 		}
 
 		public override void PayInterest()
 		{
-		  // No interest is paid
+			// No interest is paid
 		}
 
 		private void StateChangeCheck()
 		{
-		  if (balance > upperLimit)
-		  {
-			account.State = new SilverState(this);
-		  }
+			if (balance > upperLimit)
+		  	{
+				account.State = new SilverState(this);
+		  	}
 		}
 	}
  
@@ -97,47 +97,47 @@ namespace StateDP
 
 		public SilverState(double balance, Account account)
 		{
-		  this.balance = balance;
-		  this.account = account;
-		  Initialize();
+			this.balance = balance;
+		  	this.account = account;
+		  	Initialize();
 		}
 
 		private void Initialize()
 		{
-		  // Should come from a datasource
-		  interest = 0.0;
-		  lowerLimit = 0.0;
-		  upperLimit = 1000.0;
+			// Should come from a datasource
+		  	interest = 0.0;
+		  	lowerLimit = 0.0;
+		  	upperLimit = 1000.0;
 		}
 
 		public override void Deposit(double amount)
 		{
-		  balance += amount;
-		  StateChangeCheck();
+			balance += amount;
+		  	StateChangeCheck();
 		}
 
 		public override void Withdraw(double amount)
 		{
-		  balance -= amount;
-		  StateChangeCheck();
+			balance -= amount;
+		  	StateChangeCheck();
 		}
 
 		public override void PayInterest()
 		{
-		  balance += interest * balance;
-		  StateChangeCheck();
+			balance += interest * balance;
+		  	StateChangeCheck();
 		}
 
 		private void StateChangeCheck()
 		{
-		  if (balance < lowerLimit)
-		  {
-			account.State = new RedState(this);
-		  }
-		  else if (balance > upperLimit)
-		  {
-			account.State = new GoldState(this);
-		  }
+			if (balance < lowerLimit)
+		  	{
+				account.State = new RedState(this);
+		  	}
+		  	else if (balance > upperLimit)
+		  	{
+				account.State = new GoldState(this);
+		  	}
 		}
 	}
  
@@ -149,53 +149,53 @@ namespace StateDP
 	{
 		// Overloaded constructors
 		public GoldState(State state)
-		  : this(state.Balance, state.Account)
+			: this(state.Balance, state.Account)
 		{
 		}
 
 		public GoldState(double balance, Account account)
 		{
-		  this.balance = balance;
-		  this.account = account;
-		  Initialize();
+			this.balance = balance;
+		  	this.account = account;
+		  	Initialize();
 		}
 
 		private void Initialize()
 		{
-		  // Should come from a database
-		  interest = 0.05;
-		  lowerLimit = 1000.0;
-		  upperLimit = 10000000.0;
+			// Should come from a database
+		  	interest = 0.05;
+		  	lowerLimit = 1000.0;
+		  	upperLimit = 10000000.0;
 		}
 
 		public override void Deposit(double amount)
 		{
-		  balance += amount;
-		  StateChangeCheck();
+			balance += amount;
+		  	StateChangeCheck();
 		}
 
 		public override void Withdraw(double amount)
 		{
-		  balance -= amount;
-		  StateChangeCheck();
+			balance -= amount;
+		  	StateChangeCheck();
 		}
 
 		public override void PayInterest()
 		{
-		  balance += interest * balance;
-		  StateChangeCheck();
+			balance += interest * balance;
+		  	StateChangeCheck();
 		}
 
 		private void StateChangeCheck()
 		{
-		  if (balance < 0.0)
-		  {
-			account.State = new RedState(this);
-		  }
-		  else if (balance < lowerLimit)
-		  {
-			account.State = new SilverState(this);
-		  }
+			if (balance < 0.0)
+		  	{
+				account.State = new RedState(this);
+		  	}
+		  	else if (balance < lowerLimit)
+		  	{
+				account.State = new SilverState(this);
+		  	}
 		}
 	}
 	
@@ -210,46 +210,46 @@ namespace StateDP
 		// Constructor
 		public Account(string owner)
 		{
-		  // New accounts are 'Silver' by default
-		  this._owner = owner;
-		  this._state = new SilverState(0.0, this);
+			// New accounts are 'Silver' by default
+		  	this._owner = owner;
+		  	this._state = new SilverState(0.0, this);
 		}
 
 		// Properties
 		public double Balance
 		{
-		  get { return _state.Balance; }
+			get { return _state.Balance; }
 		}
 
 		public State State
 		{
-		  get { return _state; }
-		  set { _state = value; }
+			get { return _state; }
+		  	set { _state = value; }
 		}
 
 		public void Deposit(double amount)
 		{
-		  _state.Deposit(amount);
-		  Console.WriteLine("Deposited {0:C} --- ", amount);
-		  Console.WriteLine(" Balance = {0:C}", this.Balance);
-		  Console.WriteLine(" Status = {0}", this.State.GetType().Name);
-		  Console.WriteLine("");
+			_state.Deposit(amount);
+		  	Console.WriteLine("Deposited {0:C} --- ", amount);
+		  	Console.WriteLine(" Balance = {0:C}", this.Balance);
+		  	Console.WriteLine(" Status = {0}", this.State.GetType().Name);
+		  	Console.WriteLine("");
 		}
 
 		public void Withdraw(double amount)
 		{
-		  _state.Withdraw(amount);
-		  Console.WriteLine("Withdrew {0:C} --- ", amount);
-		  Console.WriteLine(" Balance = {0:C}", this.Balance);
-		  Console.WriteLine(" Status = {0}\n", this.State.GetType().Name);
+			_state.Withdraw(amount);
+		  	Console.WriteLine("Withdrew {0:C} --- ", amount);
+		  	Console.WriteLine(" Balance = {0:C}", this.Balance);
+		  	Console.WriteLine(" Status = {0}\n", this.State.GetType().Name);
 		}
 
 		public void PayInterest()
 		{
-		  _state.PayInterest();
-		  Console.WriteLine("Interest Paid --- ");
-		  Console.WriteLine(" Balance = {0:C}", this.Balance);
-		  Console.WriteLine(" Status = {0}\n", this.State.GetType().Name);
+			_state.PayInterest();
+		  	Console.WriteLine("Interest Paid --- ");
+		  	Console.WriteLine(" Balance = {0:C}", this.Balance);
+		  	Console.WriteLine(" Status = {0}\n", this.State.GetType().Name);
 		}
 	}
 	
@@ -258,9 +258,9 @@ namespace StateDP
 		public static void Main(string[] args)
 		{
 			// Open a new account
-      		Account account = new Account("Jim Johnson");
+      			Account account = new Account("Jim Johnson");
 			
-		    // Apply financial transactions
+		    	// Apply financial transactions
 			account.Deposit(500.0);
 			account.Deposit(300.0);
 			account.Deposit(550.0);
