306f391f182999fd10abb88290c01343eb977e7a
nesta-bg <nenadnesta@gmail.com>
2021-06-21 10:27:24 +0200

    Update SingletonDesignPattern.cs

--- Design Patterns/Creational Design Patterns/SingletonDesignPattern.cs
+++ Design Patterns/Creational Design Patterns/SingletonDesignPattern.cs
@@ -15,31 +15,31 @@ namespace Logger
         	{
         	}
 		
-        private static readonly Lazy<Log> instance = new Lazy<Log>(() => new Log());
+        	private static readonly Lazy<Log> instance = new Lazy<Log>(() => new Log());
 
-        public static Log GetInstance
-        {
-        	get
-            	{
-                	return instance.Value;
-            	}
-        }
+        	public static Log GetInstance
+        	{
+        		get
+            		{
+                		return instance.Value;
+            		}
+        	}
 
-        public void LogException(string message)
-        {
-        	string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
-            	string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
-            	StringBuilder sb = new StringBuilder();
-		sb.AppendLine("----------------------------------------");
-		sb.AppendLine(DateTime.Now.ToString());
-		sb.AppendLine(message);
-		using (StreamWriter writer = new StreamWriter(logFilePath, true))
-		{
-			writer.Write(sb.ToString());
-			writer.Flush();
-		}
-        }
-    }
+        	public void LogException(string message)
+        	{
+        		string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
+            		string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            		StringBuilder sb = new StringBuilder();
+			sb.AppendLine("----------------------------------------");
+			sb.AppendLine(DateTime.Now.ToString());
+			sb.AppendLine(message);
+			using (StreamWriter writer = new StreamWriter(logFilePath, true))
+			{
+				writer.Write(sb.ToString());
+				writer.Flush();
+			}
+        	}
+    	}
 }
 
 //===============================================================================//
