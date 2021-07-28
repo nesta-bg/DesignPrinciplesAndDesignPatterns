66eb2746e0569539c4815e3406f72e589fe7ae03
nesta-bg <nenadnesta@gmail.com>
2021-07-28 17:46:20 +0200

    Update InterpreterDesignPattern.cs

--- Design Patterns/Behavioral Design Patterns/InterpreterDesignPattern.cs
+++ Design Patterns/Behavioral Design Patterns/InterpreterDesignPattern.cs
@@ -1,3 +1,5 @@
+//=======================FIRST EXAMPLE=================================//
+
 using System;
 using System.Collections.Generic;
 
@@ -152,3 +154,101 @@ namespace InterpreterdDP
 		}
 	}
 }
+
+//=======================SECOND EXAMPLE=================================//
+
+using System;
+using System.Collections.Generic;
+
+namespace InterpreterDesignPattern
+{
+	public class Context
+   	{
+        	public string expression { get; set; }
+        	public DateTime date { get; set; }
+        
+		public Context(DateTime date)
+        	{
+            		this.date = date;
+        	}
+    	}
+	
+	//=======================================================//
+	
+	public interface AbstractExpression
+    	{
+        	void Evaluate(Context context);
+    	}
+	
+	public class DayExpression : AbstractExpression
+    	{
+        	public void Evaluate(Context context)
+        	{
+            		string expression = context.expression;
+            		context.expression = expression.Replace("DD", context.date.Day.ToString());
+        	}
+    	}
+	
+	public class MonthExpression : AbstractExpression
+    	{
+        	public void Evaluate(Context context)
+        	{
+            		string expression = context.expression;
+            		context.expression = expression.Replace("MM", context.date.Month.ToString());
+        	}
+    	}
+	
+	public class YearExpression : AbstractExpression
+    	{
+        	public void Evaluate(Context context)
+        	{
+            		string expression = context.expression;
+            		context.expression = expression.Replace("YYYY", context.date.Year.ToString());
+        	}
+    	}
+	
+	class SeparatorExpression : AbstractExpression
+    	{
+        	public void Evaluate(Context context)
+        	{
+            		string expression = context.expression;
+            		context.expression = expression.Replace(" ", "-");
+        	}
+    	}
+		
+	public class Program
+	{
+		public static void Main()
+		{
+            		Context context = new Context(DateTime.Now);
+            		Console.WriteLine("Please select the Expression  : MM DD YYYY or YYYY MM DD or DD MM YYYY ");
+            		context.expression = Console.ReadLine();
+            
+			List<AbstractExpression> objExpressions = new List<AbstractExpression>();
+			string[] strArray = context.expression.Split(' ');
+            		foreach(var item in strArray)
+            		{
+                		if(item == "DD")
+                		{
+                    			objExpressions.Add(new DayExpression());
+                		}
+                		else if (item == "MM")
+                		{
+                    			objExpressions.Add(new MonthExpression());
+                		}
+                		else if (item == "YYYY")
+                		{
+                    			objExpressions.Add(new YearExpression());
+                		}
+            		}
+            		objExpressions.Add(new SeparatorExpression());
+            		foreach(var obj in objExpressions)
+            		{
+                		obj.Evaluate(context);
+            		}
+            		Console.WriteLine(context.expression);
+		}
+	}
+}
+
+
